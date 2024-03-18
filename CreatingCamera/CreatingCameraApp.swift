9c2c50f99ccfebf8d4c986db31ae8dc5f95e412c
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-03-18 14:42:02 -0700

    first commit

---
+++ CreatingCamera/CreatingCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingCameraApp.swift
+//  CreatingCamera
+//
+//  Created by Brandon Johns on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CreatingCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
